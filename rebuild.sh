b38fa869649eb361a6322eb37de14caed9ab5ae8
atouba <anasstouba5@gmail.com>
2023-09-03 16:18:56 +0100

    (git add -u is useful); stage all deleted files to be committed

--- rebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo rm -rf /home/atouba/data/mariadb/*
-sudo rm -rf /home/atouba/data/wordpress/*
-docker compose down
-docker volume rm -f $(docker volume ls -q)
-
-docker compose up --build
